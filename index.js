ca6d10bec7cf2a60f8a1774732ff3011edcdcdd3
Lambda <5417634+Lambda11@users.noreply.github.com>
2019-06-15 00:37:19 +0300

    now recognizes your pet when it spawns
    opcodes are mapped by default

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ module.exports = function pet_skills(mod) {
 	let config = require('./config.json');
 	let petId = 0n;
 	
+	mod.game.initialize("me");
+	
     mod.hook('C_START_SERVANT_ACTIVE_SKILL', 1, event => {
 		petId = event.gameId;
 		if(config.ReplaceAbility && ((event.skill >= 1101 && event.skill < 1138) || event.skill === 1001))
@@ -24,13 +26,23 @@ module.exports = function pet_skills(mod) {
 			}
 			else
 			{
-				mod.command.message("You need to use the partner skill first as a sample");
+				mod.command.message("Partner not found - You need to summon him to use his partner skill");
 			}
 			return false;
 		}
 	});
 	
-	mod.hook('S_LOGIN', 'raw', event => {
-		petId = 0n;
+	mod.hook('S_REQUEST_SPAWN_SERVANT', 1, event => {
+		if(event.ownerId === mod.game.me.gameId)
+		{
+			petId = event.gameId;
+		}
+	});
+	
+	mod.hook('S_REQUEST_DESPAWN_SERVANT', 1, event => {
+		if(event.gameId === petId)
+		{
+			petId = 0n;
+		}
 	});
 }
